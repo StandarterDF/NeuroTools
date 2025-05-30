b47b1ac8c03306d789a3459fb60da7c273868bdf
DefaultDF <defaultdfhack@gmail.com>
2025-05-30 12:12:25 +0300

    Version 1.0.0 (GIT STARTED!)

--- LLMLibrary/ProjectSearcher.py
+++
@@ -1,8 +0,0 @@
-import requests
-from duckduckgo_search import DDGS
-
-class ProjectSearcher:
-    def Search(self, Text):
-        return DDGS().text(Text, max_results=5)
-    def Scrap(self, URL):
-        return requests.get(url=URL)
